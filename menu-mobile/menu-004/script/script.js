982d54b3a20bc134d912d447026c7c6a85986973
Lucas Renan <lucasrenandns96@gmail.com>
2022-06-24 12:20:39 -0300

    Update script.js

--- menu-mobile/menu-004/script/script.js
+++ menu-mobile/menu-004/script/script.js
@@ -1,9 +1,14 @@
 
+//selecionando tag ul
 let menu = document.querySelector('nav ul')
 
+//selecionando classe (menu-icon)
 let menuIcon = document.querySelector('.menu-icon')
 
-function abrirMenu() {
+function abrirMenu() {  
+
+    /* toggle = se tiver a classe "open" remova
+    Se não tiver adiciona */
 
     menu.classList.toggle('open')
 }
